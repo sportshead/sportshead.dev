caef38ad2e83da4a1dfc103370435b94e31593b9
sportshead <me@sportshead.dev>
2023-11-25 17:36:27 +0000

    config.ts: update description, domain

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://sportshead.dev/", // replace this with your deployed domain
   author: "sportshead",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  desc: "Personal blog and portfolio of @sportshead",
   title: "sportshead.dev",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
