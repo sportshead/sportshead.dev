7fec7d7f21aa0fc71669fc65dcae6c02446512ad
sportshead <me@sportshead.dev>
2023-11-27 16:32:43 +0000

    generateOgImages: replace 1001fonts url with github.com/google/fonts

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -7,13 +7,13 @@ import siteOgImage from "./og-templates/site";
 const fetchFonts = async () => {
   // Regular Font
   const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf",
+    "https://github.com/google/fonts/raw/main/ofl/ibmplexmono/IBMPlexMono-Regular.ttf",
   );
   const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
 
   // Bold Font
   const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf",
+    "https://github.com/google/fonts/raw/main/ofl/ibmplexmono/IBMPlexMono-Bold.ttf",
   );
   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
 
