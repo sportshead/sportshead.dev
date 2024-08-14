acf30eafc8ad6ac9d8ba1042fe4af0a70bc9675a
sportshead <me@sportshead.dev>
2024-08-14 20:21:32 +0800

    draft 2

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 import mdx from "@astrojs/mdx";
 
+import { transformerNotationDiff } from "shikiji-transformers";
 import { bundledLanguages } from "shikiji";
 import { readFile } from "fs/promises";
 
@@ -46,12 +47,12 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      experimentalThemes: {
+      // @ts-expect-error
+      themes: {
         light: "github-light",
         dark: "github-dark",
       },
       wrap: true,
-      // @ts-ignore
       langs: [
         ...Object.keys(bundledLanguages),
         {
@@ -60,6 +61,7 @@ export default defineConfig({
           scopeName: "source.puml",
         },
       ],
+      transformers: [transformerNotationDiff()],
     },
     smartypants: false,
   },
